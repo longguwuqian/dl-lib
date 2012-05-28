3ffe870a413a92b8455656312f7858d184d88ad4
longguwuqian <longguwuqian@gmail.com>
2012-05-28 14:58:17 +0800

    增加了测试程序

---
+++ test_vector.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "dl_lib/dl_vector.h"
+#include "dl_lib/dl_stack.h"
+
+DL_VECTOR_TEMPLATE(vector_t, int)
+
+int main()
+{
+    DL_VECTOR(vector_t, v)
+    int i;
+    DL_VECTOR_INIT(vector_t, &v)
+    for (i = 0; i < 10; i++) {
+        v.push_back(&v, i);
+    }
+    fprintf(stderr, "v.size = %d\n", v.size(&v));
+    for (i = 0; i < v.size(&v); i++) {
+        printf("v[%d] = %d\n", i, v.at(&v, i));
+    }
+    v.clear(&v);
+    printf("now, v.size = %d\n", v.size(&v));
+    v.destroy(&v);
+    return 0;
+}
+
