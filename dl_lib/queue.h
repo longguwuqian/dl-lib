8f2e6f860ca54e1bf9e3fecc7febb76a0d8188d5
longguwuqian <longguwuqian@gmail.com>
2012-06-02 14:28:53 +0800

    fix bugs in queue.h

--- dl_lib/queue.h
+++ dl_lib/queue.h
@@ -1,5 +1,8 @@
-#ifndef _DL_COMMON_DL_QUEUE_H
-#define _DL_COMMON_DL_QUEUE_H
+#ifndef _DL_LIB_QUEUE_H
+#define _DL_LIB_QUEUE_H
+
+#include <assert.h>
+
 struct __queue_node {
     struct __queue_node *next;
 };
@@ -17,38 +20,44 @@ struct queue {
 
 static inline void init_queue(struct queue *queue)
 {
-    queue->head = queue->tail = 0;
+    queue->head = queue->tail = NULL;
     queue->length = 0;
 }
 
 static inline int queue_empty(const struct queue *queue)
 {
-    return queue->head == 0;
+    assert(queue != NULL);
+    assert((queue->head == NULL && queue->tail == NULL) || (queue->head != NULL) || (queue->head == NULL && queue->length == 0));
+    return queue->head == NULL;
 }
 
 static inline void queue_add(queue_node *node,struct queue *queue)
 {
-        if(queue_empty(queue)) {
-                queue->head = queue->tail = node;
-        }
-        else {
-                queue->tail->next = node;
-                queue->tail = node;
-        }
-        queue->length++;
+    assert(node != NULL && queue != NULL);
+    if(queue_empty(queue)) {
+        queue->head = queue->tail = node;
+    } else {
+        queue->tail->next = node;
+        queue->tail = node;
+    }
+    queue->length++;
 }
 static inline queue_node* queue_del(struct queue *queue)
 {
-        queue_node *head = queue->head;
-        queue->head = head->next;
-        queue->length--;
-        return head;
+    assert(queue != NULL);
+    queue_node *old_head = queue->head;
+    if (queue_empty(queue)) return NULL;
+    queue->head = old_head->next;
+    queue->length--;
+    if (queue->length == 0) queue->head = queue->tail = NULL;
+    return old_head;
 }
 
 static inline int queue_length(struct queue *queue)
 {
-
-        return queue->length;
+    assert(queue != NULL);
+    assert(queue->length >= 0);
+    return queue->length;
 }
 
 #endif
